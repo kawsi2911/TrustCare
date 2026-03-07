cbfbc1aac16cbb96056bbfc156914c742be3048c
kawsi2911 <sof21b106@uovt.ac.lk>
2026-03-07 09:59:55 +0530

    update

--- Final year Project/Trust_Care_Front_End/src/Header/Header.jsx
+++ Final year Project/Trust_Care_Front_End/src/Header/Header.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import logo from '../assets/logo.jpeg';
+import logo from '../assets/logo.png';
 import './Header.css';
 
 function Header() {
@@ -8,7 +8,7 @@ function Header() {
       <div className = 'header-container'>
         <img src = {logo} alt = 'Logo' className = 'logo' />
         <div>
-          <h1 className = 'heading'>Trust Care System – Health Care1</h1>
+          <h1 className = 'heading'>Trust Care System – Health Care</h1>
           <p className = 'sentences'>Connecting Hearts with Helping Hands</p>
         </div>
       </div>
