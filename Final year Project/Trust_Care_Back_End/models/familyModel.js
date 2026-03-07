2df4f50cac621cbed6f7d8ad72d64d0110df2ea8
kawsi2911 <sof21b106@uovt.ac.lk>
2026-03-07 14:57:21 +0530

    update table connect

--- Final year Project/Trust_Care_Back_End/models/familyModel.js
+++ Final year Project/Trust_Care_Back_End/models/familyModel.js
@@ -28,14 +28,6 @@ const userSchema = new mongoose.Schema({
   city: {
     type: String,
     required: true
-  },
-  username: {
-    type: String,
-    required: true
-  },
-  password: {
-    type: String,
-    required: true
   }
 });
 
